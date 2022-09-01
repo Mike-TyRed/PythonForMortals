cbee802db631bebe789230f32738492b2b6230f1
Miguel T. Rojo <miguel.terrazas193@tectijuana.edu.mx>
2022-09-01 00:18:19 -0700

    Initial commit

---
+++ tiposDatos.py
@@ -0,0 +1,25 @@
+#Integers
+
+""" a = 34
+b = 76
+
+print(a+b)
+
+print(a/b)
+print(a//b) L shift + alt + a"""
+
+#Float
+
+""" f =23.89
+p = 45.32
+
+print(f * p) """
+
+#Boolean
+
+t = True
+f = False
+
+print(t , f)
+
+
